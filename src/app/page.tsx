48e4db829214db40c764e452b890356704dfc37a
freitaseric <freitaseric@proton.me>
2024-06-07 12:39:03 -0400

    fix: large desktop layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,24 +11,28 @@ export default function Home() {
   return (
     <>
       <AppHeader />
-      <main className="flex flex-col justify-center items-center gap-16 md:flex-row md:justify-around md:gap-0">
+      <main className="flex flex-col justify-center items-center gap-16 md:flex-row md:justify-around md:items-start lg:mt-48 md:gap-0 lg:justify-evenly">
         <div className="flex flex-col justify-center items-center">
-          <h1 className="font-bold text-2xl mdtext-3xl">
+          <h1 className="font-bold text-2xl md:text-3xl lg:text-4xl">
             Ciência da Computação
           </h1>
-          <h2 className="flex flex-row justify-between items-center gap-4 text-md">
+          <h2 className="flex flex-row justify-between items-center gap-4 text-base md:text-lg lg:text-xl">
             <span>8 semestres</span>{' '}
             <Separator orientation="vertical" className="h-4" />{' '}
             <span>4 anos</span>
           </h2>
         </div>
         <div className="flex flex-col justify-center items-center w-screen md:w-auto">
-          <Accordion type="single" collapsible className="w-[75%] md:w-96">
+          <Accordion
+            type="single"
+            collapsible
+            className="w-[75%] md:w-96 lg:w-[32rem]"
+          >
             <AccordionItem value="1">
-              <AccordionTrigger>
+              <AccordionTrigger className="lg:text-xl">
                 O que posso aprender no curso?
               </AccordionTrigger>
-              <AccordionContent className="text-wrap text-justify">
+              <AccordionContent className="text-wrap text-justify lg:text-lg">
                 No curso de ciência da computação as matérias principais, ou
                 seja, as específicas deste curso, são{' '}
                 <strong>Algoritmos</strong>,{' '}
@@ -39,8 +43,10 @@ export default function Home() {
               </AccordionContent>
             </AccordionItem>
             <AccordionItem value="2">
-              <AccordionTrigger>Onde posso achar o curso?</AccordionTrigger>
-              <AccordionContent className="text-wrap text-justify">
+              <AccordionTrigger className="lg:text-xl">
+                Onde posso achar o curso?
+              </AccordionTrigger>
+              <AccordionContent className="text-wrap text-justify lg:text-lg">
                 Algumas universidades públicas renomadas que oferecem o curso de
                 Ciência da Computação são as{' '}
                 <strong>universidades federais</strong> de diversos estados,
@@ -52,10 +58,10 @@ export default function Home() {
               </AccordionContent>
             </AccordionItem>
             <AccordionItem value="3">
-              <AccordionTrigger>
+              <AccordionTrigger className="lg:text-xl">
                 Como seria o mercado de trabalho?
               </AccordionTrigger>
-              <AccordionContent className="text-wrap text-justify">
+              <AccordionContent className="text-wrap text-justify lg:text-lg">
                 O mercado de trabalho para um cientista da computação é bastante
                 promissor, com salários que podem atingir até{' '}
                 <strong>R$ 12.000,00</strong> (doze mil reais), dependendo da
